6fe9e94f350c7d35eb9312fd3ca158db8f69683a
mazenalhabeby <a.mazen.dessouky@gmail.com>
2026-01-10 22:52:50 +0100

    edit docker file

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module, Logger } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -21,30 +21,6 @@ import { EmailModule } from '../email/email.module';
 import { WalletModule } from '../wallet/wallet.module';
 import { NotificationsModule } from '../notifications/notifications.module';
 import { ACCESS_TOKEN_EXPIRY } from '../../config/cookie.config';
-
-// Conditional provider factory for OAuth strategies
-const createOAuthProviders = () => {
-  const logger = new Logger('AuthModule');
-  const providers: any[] = [];
-
-  // Google OAuth - only register if credentials are provided
-  if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
-    providers.push(GoogleStrategy);
-    logger.log('Google OAuth strategy enabled');
-  } else {
-    logger.warn('Google OAuth disabled: GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET not set');
-  }
-
-  // Facebook OAuth - only register if credentials are provided
-  if (process.env.FACEBOOK_APP_ID && process.env.FACEBOOK_APP_SECRET) {
-    providers.push(FacebookStrategy);
-    logger.log('Facebook OAuth strategy enabled');
-  } else {
-    logger.warn('Facebook OAuth disabled: FACEBOOK_APP_ID or FACEBOOK_APP_SECRET not set');
-  }
-
-  return providers;
-};
 
 @Module({
   imports: [
@@ -74,7 +50,8 @@ const createOAuthProviders = () => {
     JwtStrategy,
     JwtRefreshStrategy,
     LocalStrategy,
-    ...createOAuthProviders(),
+    GoogleStrategy,
+    FacebookStrategy,
   ],
   controllers: [AuthController],
   exports: [AuthService, TokenService, SessionService, OAuthService, WalletManagementService, TwoFactorService],
